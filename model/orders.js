1d9bf90d8bc6f4364f7ab2cdf373c2627d0cc30d
govinda1999 <gspatel61199@gmail.com>
2020-07-03 19:12:30 +0530

    [Govinda] | Added Orders Schema

---
+++ model/orders.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const User = require('./userModel');
+const Item = require('./fooditem');
+
+const ordersSchema = mongoose.Schema({
+  date: { type: Date, default: new Date() },
+  order: { type: String, required: true },
+  status: { type: String },
+  payment: { type: Number, required: true },
+  item: { type: [mongoose.Schema.Types.ObjectId], ref: Item, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: User, required: true },
+  payment_mode: { type: String, required: true },
+  payment_done: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model('orders', ordersSchema);
