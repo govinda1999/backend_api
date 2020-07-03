ef10a2852bb4149dd808956409e2cdbd4a2938e7
govinda1999 <gspatel61199@gmail.com>
2020-07-03 17:11:37 +0530

    [Govinda] | Added Routes for Food Items

---
+++ routes/fooditem.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const router = express.Router();
+const Food = require('../model/fooditem');
+const Token = require('../model/authenticationToken');
+
+router.post('/', async (req, res) => {
+  const { name, imageUrl, rate, time, description, price, category } = req.body;
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(403).json({
+      message: 'Token is Required',
+      statusCode: 403,
+    });
+  }
+  const user = await Token.findOne({ token }).exec();
+  if (!user) {
+    res.status(403).json({
+      message: 'Token is Invalid',
+      statusCode: 403,
+    });
+  }
+
+  const newFood = new Food({
+    name,
+    imageUrl,
+    rate,
+    time,
+    description,
+    price,
+    category,
+  });
+  newFood
+    .save()
+    .then((doc) => {
+      res.status(200).json({
+        message: 'Food Added Successfully',
+        response: doc,
+        statusCode: 200,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Error in Food' + err, statusCode: 500 });
+    });
+});
+
+router.get('/', async (req, res) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(403).json({
+      message: 'Token is Required',
+      statusCode: 403,
+    });
+  }
+
+  const user = await Token.findOne({ token }).exec();
+  if (!user) {
+    res.status(403).json({
+      message: 'Token is Invalid',
+      statusCode: 403,
+    });
+  }
+
+  Food.find()
+    .exec()
+    .then((doc) => {
+      res.status(200).json({
+        message: 'List of Food',
+        response: doc,
+        statusCode: 200,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'Error in Food' + err, statusCode: 500 });
+    });
+});
+
+router.delete('/:id', async (req, res) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(403).json({
+      message: 'Token is Required',
+      statusCode: 403,
+    });
+  }
+
+  const user = await Token.findOne({ token }).exec();
+  if (!user) {
+    res.status(403).json({
+      message: 'Token is Invalid',
+      statusCode: 403,
+    });
+  }
+
+  const id = req.params.id;
+
+  Food.deleteOne({ _id: id })
+    .exec()
+    .then((doc) => {
+      res.status(200).json({
+        message: 'Food is deleted from System',
+        statusCode: 200,
+      });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: 'Error in Delete Food' + err, statusCode: 500 });
+    });
+});
+
+router.put('/:id', async (req, res) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(403).json({
+      message: 'Token is Required',
+      statusCode: 403,
+    });
+  }
+
+  const user = await Token.findOne({ token }).exec();
+  if (!user) {
+    res.status(403).json({
+      message: 'Token is Invalid',
+      statusCode: 403,
+    });
+  }
+
+  const id = req.params.id;
+
+  Food.updateOne({ _id: id }, req.body)
+    .exec()
+    .then((doc) => {
+      res.status(200).json({
+        message: 'Item is Updated in System',
+        statusCode: 200,
+      });
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: 'Error in Update Item' + err, statusCode: 500 });
+    });
+});
+
+module.exports = router;
